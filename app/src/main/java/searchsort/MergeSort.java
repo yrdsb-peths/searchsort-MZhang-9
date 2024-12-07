7f9e22d1b3c8a8346c3f382a023b4e79242616e2
MZhang-9 <m2volleyballa@gmail.com>
2024-12-06 19:48:59 -0500

    merge sort

--- app/src/main/java/searchsort/MergeSort.java
+++ app/src/main/java/searchsort/MergeSort.java
@@ -6,13 +6,44 @@ public class MergeSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
+        if (arr.length < 2) {
+            return; 
+        }
 
+        
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+        sort(left);
+        sort(right);
+
+        merge(arr, left, right);
     }
 
     /*
      * Merge the two sorted arrays left and right into the array arr.
      */
     private void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
 
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
     }
 }
